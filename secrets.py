34ab7c07b65de92f73a3012f2f5ca4a604d62e0e
Rachelle Mehdi <mehdir@umich.edu>
2018-11-26 21:47:46 -0500

    Starter Code
    These files contain starter code that was given for Project 2. I have edited the secrets file with my own plotly/google places/mapbox information.

---
+++ secrets.py
@@ -0,0 +1,8 @@
+# put your google api key here
+google_places_key = 'AIzaSyCRqg6FLBfvzmepFuCa3Bq3FCK35nonlgc'
+
+
+# Plotly username and api_key here
+PLOTLY_USERNAME = "mehdir"
+PLOTLY_API_KEY = "s17MMb2XUvnuuqOMJhXB"
+MAPBOX_TOKEN = "pk.eyJ1IjoicHJpbmNpcGxleiIsImEiOiJjam1taTE3dGowamRjM3FqcG50MGp0anEwIn0.XuaFZy4Tff6aTfjiQUdd9Q"

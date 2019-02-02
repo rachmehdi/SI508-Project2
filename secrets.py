f8f198473d33658d10294df60d0d91d4d740b9f8
rachmehdi <44327409+rachmehdi@users.noreply.github.com>
2019-02-01 23:16:17 -0500

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,8 +1,8 @@
 # put your google api key here
-google_places_key = 'AIzaSyCRqg6FLBfvzmepFuCa3Bq3FCK35nonlgc'
+google_places_key = ''
 
 
 # Plotly username and api_key here
-PLOTLY_USERNAME = "mehdir"
-PLOTLY_API_KEY = "s17MMb2XUvnuuqOMJhXB"
-MAPBOX_TOKEN = "pk.eyJ1IjoicHJpbmNpcGxleiIsImEiOiJjam1taTE3dGowamRjM3FqcG50MGp0anEwIn0.XuaFZy4Tff6aTfjiQUdd9Q"
+PLOTLY_USERNAME = ""
+PLOTLY_API_KEY = ""
+MAPBOX_TOKEN = ""
